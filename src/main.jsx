3c4a72f2d1baba117ba54df754dc355cbdc07015
Noam <noam@example.com>
2025-12-30 15:30:57 +0200

    Initial commit - Imposter Game fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import App from './App.jsx'
+import './index.css'
+// 1. ייבוא הכלים של React Query
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// 2. יצירת מופע חדש של הקליינט
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-) 
+  <React.StrictMode>
+    {/* 3. עטיפת האפליקציה ב-Provider */}
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
