5c4d2f6859750a42d0723ad4afd6c9dd96a1d611
Noam <noam@example.com>
2025-12-30 16:53:00 +0200

    dfnm

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -1,4 +1,4 @@
-import { db } from "./firebase";
+import { db } from "../firebase";
 import { collection, addDoc, getDocs, query, where, doc, updateDoc, getDoc, arrayUnion } from "firebase/firestore";
 
 const ROOMS_COLLECTION = 'rooms';
